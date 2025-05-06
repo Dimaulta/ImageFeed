0a65bcffd35b6a72b0e32ecbfcac0c1c21dbd9c5
Ульта <lightpaintru@gmail.com>
2025-05-06 13:34:45 +0300

    Удалил из экрана профиля UI созданный через interface builder Сверстал экран профиля кодом

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,13 +8,83 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet private var avatarImageView: UIImageView!
-    @IBOutlet private var nameLabel: UILabel!
-    @IBOutlet private var loginNameLabel: UILabel!
-    @IBOutlet private var descriptionLabel: UILabel!
-
-    @IBOutlet private var logoutButton: UIButton!
-
     @IBAction private func didTapLogoutButton() {
+    }
+    
+    private var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let avatarImageView = UIImageView(image: UIImage(named: "Avatar"))
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let userNameLabel = UILabel()
+        userNameLabel.text = "Екатерина Новикова"
+        userNameLabel.textColor = UIColor(named: "YP White")
+        userNameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let loginNameLabel = UILabel()
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.textColor = UIColor(named: "YP Gray")
+        loginNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = UIColor(named: "YP White")
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let exitButton = UIButton.systemButton(
+            with: UIImage(named: "Exit")!.withRenderingMode(.alwaysOriginal),
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(avatarImageView)
+        view.addSubview(userNameLabel)
+        view.addSubview(loginNameLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(exitButton)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+            
+            userNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            userNameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 8),
+            
+            loginNameLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            loginNameLabel.trailingAnchor.constraint(equalTo: userNameLabel.trailingAnchor),
+            loginNameLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
+            loginNameLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -8),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: userNameLabel.trailingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            exitButton.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            exitButton.widthAnchor.constraint(equalToConstant: 44),
+            exitButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    @objc
+    private func didTapButton() {
+        label?.removeFromSuperview()
+        label = nil
+        
+        for view in view.subviews {
+            if view is UILabel {
+                view.removeFromSuperview()
+            }
+        }
     }
 }
