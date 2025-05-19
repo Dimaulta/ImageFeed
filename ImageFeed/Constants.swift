e34e92e74d11222cd9a73c399f2849afef6d33ec
Ульта <lightpaintru@gmail.com>
2025-05-19 10:24:22 +0300

    Зарегистрировал приложение сервисе unsplash Добавил Asses Kay Secret Kay redirect URI Access scope и Default Base URL Добавил webview и processbar

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift.swift
+//  ImageFeed
+//
+//  Created by Ульта on 16.05.2025.
+//
+
+import UIKit
+
+enum Constants {
+    
+    static let accessKey = "dUaraL4pnNKKk33SBBErYg7636WPwKbCDkx3N5y5mTo"
+    static let secretKey = "EFynbwdcTNzT_feE-HU7yKnldnnML1U_79PbeNztVZg"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+     
+}
