371f2c12d048c457c4d104b2050e61cbd5c1d8f4
Ульта <lightpaintru@gmail.com>
2025-05-25 13:33:31 +0300

    Убрал force unwrap операции Использовал безопасное извлечение опционалов через guard let Применил do-catch для обработки ошибок

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -13,6 +13,12 @@ enum Constants {
     static let secretKey = "EFynbwdcTNzT_feE-HU7yKnldnnML1U_79PbeNztVZg"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    
+    static var defaultBaseURL: URL {
+        guard let url = URL(string: "https://api.unsplash.com") else {
+            fatalError("Failed to create base URL")
+        }
+        return url
+    }
      
 }
