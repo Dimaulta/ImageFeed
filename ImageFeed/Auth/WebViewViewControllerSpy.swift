9bd69cc3d5a101ea4df02d6b6ad7706a0ce6d3b1
Ульта <lightpaintru@gmail.com>
2025-07-09 14:11:02 +0300

    test: добавил unit тесты для WebView компонентов
    - Создал WebViewPresenterSpy и WebViewViewControllerSpy для тестирования
    - Добавил тесты связи контроллера и презентера
    - Написал тесты для проверки логики прогресса
    - Добавил тесты для AuthHelper (создание URL и парсинг кода)
    - Покрыл тестами все основные компоненты авторизации

---
+++ ImageFeed/Auth/WebViewViewControllerSpy.swift
@@ -0,0 +1,27 @@
+//
+//  WebViewViewControllerSpy.swift
+//  ImageFeed
+//
+//  Created by Ульта on 09.07.2025.
+//
+
+import ImageFeed
+import Foundation
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+
+    var loadRequestCalled: Bool = false
+
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+
+    }
+}
